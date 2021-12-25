42dc6d66b19cc33c29be7fafa09994a27c451fa9
Mason750 <maceonb@gmail.com>
2021-12-25 08:44:02 -0800

    Bagels game finished, numSize hard coded to 3

--- bagels.py
+++ bagels.py
@@ -1,38 +1,56 @@
+import random
+
 stillPlaying = True
 numSize = 3
+guess = []
 
 #Generate <numSize> random numbers range 0-9, returns as String
 def generateNum(numSize):
-    return '123'
+    num = ''
+    for i in range(numSize):
+        num+= str(random.randrange(0, 10))
+    return num
 
 #Prompt player to enter guess, returns guess
 def getGuess():
-    return '122'
+    guess = input("Enter your guess: ")
+    return guess
 
 #Check player guess with answer
 #Add Pico to list if one digit correct in wrong spot
 #Add Fermi to list if digit correct in right spot
 #Add Bagels if loop ends and list is still empty (no right numbers)
-def compare(num, guess):
-    return ['Pico', 'Fermi']
+def compare(num, guess, numSize):
+    result = []
+    for i in range(numSize):
+        if(num[i] == guess[i]):
+            result.append("Pico")
+        elif(num[i] in guess):
+            result.append("Fermi")
+    if(not result):
+        result.append("Bagels")
+    random.shuffle(result)
+    return result
 
 #Get user input for repeating game
 def playAgain():
+    playingAgain = input("Play again? (y/n)")
+    if(playingAgain == 'y'):
+        return True
     return False
 
+print("When I say:      That means:")
+print("Pico      One digit is correct but in the wrong position")
+print("Fermi     One digit is correct and in the right position")
+print("Bagels    No digit is correct")
+    
 #Main playing loop
 while(stillPlaying):
-    print("When I say:      That means:")
-    print("Pico      One digit is correct but in the wrong position")
-    print("Fermi     One digit is correct and in the right position")
-    print("Bagels    No digit is correct")
-    
     num = generateNum(numSize)
     print("I have thought of a number")
-
-    guess = getGuess()
-    
-    result = compare(num, guess)
-    print(result)
+    while(num != guess):
+        guess = getGuess()
+        result = compare(num, guess, numSize)
+        print(result)
 
     stillPlaying = playAgain()
